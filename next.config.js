a5de8b5054755641641ceb951d0f0d5801530073
darlansaulo <darlansaulo@gmail.com>
2022-02-24 04:51:30 -0300

    primeiro commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   reactStrictMode: true,
+
+    assetPrefix: isProd ? '/projeto.curriculo.nextjs/' : '',
+    
     images: {
       loader: 'akamai',
       path: '',
